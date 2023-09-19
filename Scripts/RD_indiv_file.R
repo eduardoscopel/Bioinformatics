bd2968324756973bb2a28039e6f128f29c43bed8
Eduardo Scopel Ferreira da Costa <eduardoscopel@uga.edu>
2023-09-18 20:08:23 -0500

    Organized scripts into folders

--- Scripts/RD_indiv_file.R
+++ Scripts/RD_indiv_file.R
@@ -1,48 +1,100 @@
 #!/usr/bin/env Rscript
+
+# Load required libraries
 library(data.table)
+
+# Retrieve command line arguments
 args = commandArgs(trailingOnly=TRUE)
-if (length(args)==0) {
+
+# Check if at least one argument (input file) is supplied
+if (length(args) == 0) {
   stop("At least one argument must be supplied (input file).depth", call.=FALSE)
 }
+
+# Read the input file into a data table
 depthtab <- fread(args[1], col.names = c("Chromosome", "Position", "Depth"))
+
+# Initialize counters and variables
 c = 0
 k = 0
 cstart <- 0
 cend <- 0
-g = sub(".*depth_","",args[1])
-pdf(paste0(g,"_SW_SP.pdf"))
-med = 2*median(depthtab$Depth, na.rm=TRUE)
+
+# Extract the base name of the input file for output file naming
+g = sub(".*depth_", "", args[1])
+
+# Create a PDF device for output
+pdf(paste0(g, "_SW_SP.pdf"))
+
+# Calculate the median depth for later use
+med = 2 * median(depthtab$Depth, na.rm = TRUE)
+
+# Define chromosome grouping
 a <- unique(depthtab$Chromosome)
 a[5:8] <- unique(depthtab$Chromosome)[7:10]
 a[9] <- unique(depthtab$Chromosome)[5]
 a[10:16] <- unique(depthtab$Chromosome)[11:17]
 a[17] <- unique(depthtab$Chromosome)[6]
-op <- par(mar=c(1,1,4,1)+0.1, oma = c(5,4,4,1)+0.1, mfcol=c(1,16))
-for(i in a){
-	if(grepl("chrM", i)){next}
-	chrm <- depthtab[depthtab$Chromosome == i,2:3]
-  attach(chrm)
-  W <- 1:as.integer(max(Position)/1000)*1000
-  W <- append(W,max(W)+1000)
-  meanDepth <- 0
-  for(j in 1:length(W)){meanDepth[j] <- mean(Depth[Position>(W[j]-1000)&Position<=W[j]])}
-	par(mar=c(0,0,2,0))
-	if(c%%4==0){color="black"}else if(c%%4==1){color="#E44848"}else if(c%%4==2){color="#9FCDF0"}else if(c%%4==3){color="#E0F09F"}
-  plot(W[1:max(W)], meanDepth[1:max(W)],pch=19,cex=0.2, ylim=c(0,med), axes = FALSE, col= color)
-	title(main = paste0(sub(".*chr", "", i), "\n", median(Depth, na.rm = TRUE), "x"), cex.main = 1)
-	k = k + max(W)
-	axis(side = 1, at = max(W), labels = k/1000, cex.axis = 1, pos=0)
-	if(c==0){axis(side = 2, at = seq(0,med,(med/5)),pos=0)}
-  #abline(h=mean(Depth),col="red")
-  abline(h=median(Depth, na.rm = TRUE), col="blue")
-	abline(h=0, col = "black")
-	detach(chrm)
-	c=c+1
-}
-title(main = g, xlab="genomic position (kbp)", ylab="mean depth", outer = TRUE, line = 2)
-par(op)
-dev.off()
-c=0
-k=0
-rm(list=ls())
 
+# Set up plot parameters
+op <- par(mar = c(1, 1, 4, 1) + 0.1, oma = c(5, 4, 4, 1) + 0.1, mfcol = c(1, 16))
+
+# Loop through chromosomes
+for (i in a) {
+  # Skip mitochondrial chromosome
+  if (grepl("chrM", i)) {
+    next
+  }
+  
+  # Subset data for the current chromosome
+  chrm <- depthtab[depthtab$Chromosome == i, 2:3]
+  
+  # Calculate genomic windows
+  attach(chrm)
+  W <- 1:as.integer(max(Position) / 1000) * 1000
+  W <- append(W, max(W) + 1000)
+  
+  # Calculate mean depth for each window
+  meanDepth <- sapply(W, function(w) mean(Depth[Position > (w - 1000) & Position <= w]))
+  
+  # Set plot color based on counter 'c'
+  par(mar = c(0, 0, 2, 0))
+  if (c %% 4 == 0) {
+    color = "black"
+  } else if (c %% 4 == 1) {
+    color = "#E44848"
+  } else if (c %% 4 == 2) {
+    color = "#9FCDF0"
+  } else if (c %% 4 == 3) {
+    color = "#E0F09F"
+  }
+  
+  # Create the plot
+  plot(W[1:max(W)], meanDepth[1:max(W)], pch = 19, cex = 0.2, ylim = c(0, med), axes = FALSE, col = color)
+  title(main = paste0(sub(".*chr", "", i), "\n", median(Depth, na.rm = TRUE), "x"), cex.main = 1)
+  k = k + max(W)
+  axis(side = 1, at = max(W), labels = k / 1000, cex.axis = 1, pos = 0)
+  
+  if (c == 0) {
+    axis(side = 2, at = seq(0, med, (med / 5)), pos = 0)
+  }
+  
+  abline(h = median(Depth, na.rm = TRUE), col = "blue")
+  abline(h = 0, col = "black")
+  detach(chrm)
+  c = c + 1
+}
+
+# Set plot titles and labels
+title(main = g, xlab = "genomic position (kbp)", ylab = "mean depth", outer = TRUE, line = 2)
+
+# Reset plot parameters
+par(op)
+
+# Close the PDF device
+dev.off()
+
+# Reset counters and remove objects from memory
+c = 0
+k = 0
+rm(list = ls())
--- Scripts/popGenUtils/admixture.sh
+++ Scripts/popGenUtils/admixture.sh
@@ -41,25 +41,3 @@ done
 
 sed -i 's/(K=//g' *.txt
 sed -i 's/)://g' *.txt
-
-
-for K in {1..20}
-do
-  echo "#!/bin/bash" > scripts/K${K}_9999.sh
-  echo "#SBATCH --job-name=${K}_1" >> scripts/K${K}_9999.sh
-  echo "#SBATCH --partition=bensasson_p" >> scripts/K${K}_9999.sh
-  echo "#SBATCH --ntasks=1" >> scripts/K${K}_9999.sh
-  echo "#SBATCH --nodes=1" >> scripts/K${K}_9999.sh
-  echo "#SBATCH --mem=1G" >> scripts/K${K}_9999.sh
-  echo "#SBATCH --time=24:00:00" >> scripts/K${K}_9999.sh
-  echo "#SBATCH --mail-type=END,FAIL" >> scripts/K${K}_9999.sh
-  echo "#SBATCH --mail-user=eduardoscopel@uga.edu" >> scripts/K${K}_9999.sh
-  echo "#SBATCH --output=/scratch/es47540/rawdata/GWAS/diploid/admixture/output/%x_%j.out" >> scripts/K${K}_9999.sh
-  echo "#SBATCH --error=/scratch/es47540/rawdata/GWAS/diploid/admixture/error/%x_%j.err" >> scripts/K${K}_9999.sh
-  echo " " >> scripts/K${K}_9999.sh
-  echo "cd /scratch/es47540/rawdata/GWAS/diploid/admixture" >> scripts/K${K}_9999.sh
-  echo "ml ADMIXTURE/1.3.0" >> scripts/K${K}_9999.sh
-  echo "admixture --cv -s 9999 gain2n_gSNPs_q40.bed s9999/${K}" >> scripts/K${K}_9999.sh
-  chmod 755 scripts/K${K}_9999.sh
-  sbatch scripts/K${K}_9999.sh
-done
